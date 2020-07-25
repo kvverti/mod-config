4f6d4b581e71a2890075c56f2e1208f7cd6a4126
Thalia Nero <chrisvn00@gmail.com>
2020-07-24 20:56:00 -0400

    Mirror sliders in RTL mode

--- src/main/java/io/github/kvverti/modconfig/data/option/IntModOption.java
+++ src/main/java/io/github/kvverti/modconfig/data/option/IntModOption.java
@@ -3,6 +3,7 @@ package io.github.kvverti.modconfig.data.option;
 import io.github.kvverti.modconfig.data.facade.IntOptionFacade;
 import org.lwjgl.glfw.GLFW;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import net.minecraft.client.gui.widget.SliderWidget;
@@ -26,7 +27,12 @@ public class IntModOption extends ModOption<Integer> {
 
     @Override
     public AbstractButtonWidget createWidget(Screen containing, int width, int height) {
-        return new SliderWidget(0, 0, width, height, getMergedMessageText(), (double)(this.getState() - min) / max) {
+        boolean rtl = MinecraftClient.getInstance().textRenderer.isRightToLeft();
+        double initialValue = (double)(this.getState() - min) / max;
+        if(rtl) {
+            initialValue = 1.0 - initialValue;
+        }
+        return new SliderWidget(0, 0, width, height, getMergedMessageText(), initialValue) {
             @Override
             protected void updateMessage() {
                 this.setMessage(IntModOption.this.getMergedMessageText());
@@ -35,7 +41,8 @@ public class IntModOption extends ModOption<Integer> {
             @Override
             protected void applyValue() {
                 double halfStep = 1.0 / (2 * (max - min));
-                IntModOption.this.saveState((int)((this.value + halfStep) * max) + min);
+                double value = rtl ? 1.0 - this.value : this.value;
+                IntModOption.this.saveState((int)((value + halfStep) * max) + min);
             }
 
             @Override
